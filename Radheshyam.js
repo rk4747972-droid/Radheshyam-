51d89f042d546f949c3002153dbf4da34f5117b6
rk4747972-droid <rk4747972@gmail.com>
2025-12-17 14:22:34 +0530

    Create Radheshyam.js

---
+++ Radheshyam.js
@@ -0,0 +1,25 @@
+let display;
+
+window.onload = function () {
+    display = document.getElementById("display");
+};
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = "Error";
+    }
+}
